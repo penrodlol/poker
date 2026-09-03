4d6ed4599384a03f32cacea937fc45873b0feff0
penrodlol <christian@penrod.contact>
2026-09-02 20:51:01 -0400

    Fix env vars part 2

--- src/libs/discord.ts
+++ src/libs/discord.ts
@@ -1,7 +1,9 @@
 import { DiscordSDK } from '@discord/embedded-app-sdk';
-import { clientEnv } from './env';
+import { z } from 'zod';
+
+const discordClientId = z.string().parse(import.meta.env.VITE_DISCORD_CLIENT_ID);
 
 const discordGlobal = globalThis as typeof globalThis & { discord?: DiscordSDK };
 
-export const proxyDiscordUrl = (path: string) => new URL(path, `https://${clientEnv.VITE_DISCORD_CLIENT_ID}.discordsays.com`).toString();
-export default (discordGlobal.discord ??= new DiscordSDK(clientEnv.VITE_DISCORD_CLIENT_ID));
+export const proxyDiscordUrl = (path: string) => new URL(path, `https://${discordClientId}.discordsays.com`).toString();
+export default (discordGlobal.discord ??= new DiscordSDK(discordClientId));
--- src/server/fetch/index.ts
+++ src/server/fetch/index.ts
@@ -1,6 +1,7 @@
-import { getServerEnv } from '#/libs/env';
 import { logError } from '#/server/utils/logger';
 import tanstack from '@tanstack/react-start/server-entry';
+import { env } from 'cloudflare:workers';
+import { z } from 'zod';
 
 export const FETCH_ERROR = 'Error Processing Fetch';
 
@@ -9,7 +10,7 @@ const handler: ExportedHandler<Env>['fetch'] = async (request) => {
     const response = await tanstack.fetch(request);
     const headers = new Headers(response.headers);
     headers.delete('X-Frame-Options');
-    headers.set('Content-Security-Policy', getServerEnv().DISCORD_FRAME_ANCESTORS);
+    headers.set('Content-Security-Policy', z.string().parse(env.DISCORD_FRAME_ANCESTORS));
     return new Response(response.body, { status: response.status, statusText: response.statusText, headers });
   } catch (error) {
     logError(FETCH_ERROR, { error, request });
